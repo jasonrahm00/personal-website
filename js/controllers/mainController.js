14dbbeabe6e62fd0acd8c4465ac97b63d14ed53b
icesmoo00th <jasonrahm@gmail.com>
2016-04-11 21:18:14 -0600

    fixed markup on accordion to coincide with newest version of ui-bootstrap and loaded most recent stable versions of angular and ui-bootstrap

--- js/controllers/mainController.js
+++
@@ -1,7 +0,0 @@
-//personalWebsite.controller('mainController', ['$scope', '$location', 'Page', function ($scope, $location, Page) {
-//  $scope.isActive = function (route) {
-//    return route === $location.path();
-//  };
-//  $scope.Page = Page;
-//}]);
-
--- js/factories/pageTitle.js
+++
@@ -1,7 +0,0 @@
-personalWebsite.factory('Page', function(){
-  var meta = '';
-  return {
-    meta: function() { return meta; },
-    setMeta: function(newMeta) { meta = newMeta;}
-  };
-});
