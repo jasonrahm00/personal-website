b137d7a81187f9477ea3fb28ecd4781a52868482
icesmoo00th <jasonrahm@gmail.com>
2016-04-13 17:16:28 -0600

    implemented dynamic title change with solution from coderwall. This solution allows me to easily update the meta description as well

--- js/app.js
+++ js/app.js
@@ -1,8 +1,17 @@
 var personalWebsite = angular.module('personalWebsite', ['ngAnimate', 'ui.bootstrap', 'ui.router' ]);
 
+// Changes the root scope to match the new view state when the state changes.
+// Solution from the Coderwall Website: https://coderwall.com/p/zk7qww/dynamic-site-page-titles-with-angular-and-ui-router
+
+personalWebsite.run(function($rootScope, $state) {
+  $rootScope.$on('$stateChangeStart', function() {
+    $rootScope.$state = $state;
+  })
+})
+
 //ROUTES
 
-personalWebsite.config(function ($stateProvider, $urlRouterProvider) {
+personalWebsite.config(['$stateProvider', '$urlRouterProvider',  function ($stateProvider, $urlRouterProvider) {
   
   $stateProvider
     
@@ -11,23 +20,39 @@ personalWebsite.config(function ($stateProvider, $urlRouterProvider) {
     .state('home', {
       url: '',
       templateUrl: 'views/home.html',
-      controller: 'homePageController'
+      controller: 'homePageController',
+      data: {
+        title: 'Jason Rahm',
+        description: 'This website was custom-built by Jason Rahm to showcase his work history and talents.'
+      }
     })
     .state('resume', {
       url: '/resume',
       templateUrl: 'views/resume.html',
-      controller: 'resumeController'
+      controller: 'resumeController',
+      data: {
+        title: 'Jason Rahm\'s Resume',
+        description: 'See Jason\'s work and education history and his current skillset.'
+      }
     })
     .state('portfolio', {
       url: '/portfolio',
       templateUrl: 'views/portfolio.html',
-      controller: 'portfolioController'
+      controller: 'portfolioController',
+      data: {
+        title: 'Jason Rahm\'s Portfolio',
+        description: 'Peruse a sample of Jason\'s work.'
+      }
     })
     .state('contact', {
       url: '/contact',
-      templateUrl: 'views/contact.html'
+      templateUrl: 'views/contact.html',
+      data: {
+        title: 'Contact Jason Rahm',
+        description: 'Fill out the form to send an email to Jason Rahm.'
+      }
     });
   
   $urlRouterProvider.otherwise('/');
   
-});
+}]);
