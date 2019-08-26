62a0fcfee970faac5caf6c2b2fef31a4a98339d3
Jason <jasonrahm@gmail.com>
2019-08-25 19:10:23 -0700

    separate firebase dev project set up with properties added to env config file

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,18 +2,30 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 // Your web app's Firebase configuration
-var firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIRESTORE_PERSONAL_SITE_KEY,
-  authDomain: process.env.REACT_APP_FIRESTORE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIRESTORE_DB_URL,
-  projectId: process.env.REACT_APP_FIRESTORE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIRESTORE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIRESTORE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIRESTORE_APP_ID
+/*
+const firebaseProdConfig = {
+  apiKey: process.env.REACT_APP_FIRESTORE_PROD_PERSONAL_SITE_KEY,
+  authDomain: process.env.REACT_APP_FIRESTORE_PROD_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIRESTORE_PROD_DB_URL,
+  projectId: process.env.REACT_APP_FIRESTORE_PROD_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIRESTORE_PROD_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIRESTORE_PROD_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIRESTORE_PROD_APP_ID
+};
+*/
+
+const firebaseDevConfig = {
+  apiKey: process.env.REACT_APP_FIRESTORE_DEV_PERSONAL_SITE_KEY,
+  authDomain: process.env.REACT_APP_FIRESTORE_DEV_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIRESTORE_DEV_DB_URL,
+  projectId: process.env.REACT_APP_FIRESTORE_DEV_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIRESTORE_DEV_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIRESTORE_DEV_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIRESTORE_DEV_APP_ID
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseDevConfig);
 
 const db = firebase.firestore();
 
