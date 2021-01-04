dd984e468ef3361eeae404b31de0bc1fb552effb
jasonrahm00 <jasonrahm@gmail.com>
2021-01-04 11:35:22 -0800

    scrollToTop component added for route changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+import ScrollToTop from './components/base/nav/scrollToTop';
 import About from './components/pages/about';
 import Experience from './components/pages/experience';
 import MainFooter from './components/base/mainFooter';
-import MainNav from './components/base/mainNav';
+import MainNav from './components/base/nav/mainNav';
 import Skills from './components/pages/skills';
 import Education from './components/pages/education';
 
@@ -13,6 +14,7 @@ function App() {
     <Router>
       <MainNav />
       <main>
+        <ScrollToTop />
         <Switch>
           <Route path="/" exact component={About} />
           <Route path="/experience" component={Experience} />
