50a4a01aed556f6b819f6b2b060fb30f2142d813
jasonrahm00 <jasonrahm@gmail.com>
2021-01-08 09:49:33 -0800

    styling and icon applied to loading component

--- src/components/base/loading.js
+++ src/components/base/loading.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCog } from '@fortawesome/free-solid-svg-icons' ;
 
 function Loading() {
 
   return(
-    <div>Loading</div>
+    <div className="text-center">
+      <h2>Loading</h2>
+      <FontAwesomeIcon icon={faCog} size="2x" spin aria-hidden="true"/>
+    </div>
   )
 }
 
