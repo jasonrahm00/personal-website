0a2c71b771eef92427b54ff932d58d3bb5048a02
jasonrahm00 <jasonrahm@gmail.com>
2021-01-18 15:51:34 -0800

    config file changed to production entries for deployment

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,8 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 
-// Your web app's Firebase configuration
-/*
+//Production App
 const firebaseProdConfig = {
   apiKey: process.env.REACT_APP_FIRESTORE_PROD_PERSONAL_SITE_KEY,
   authDomain: process.env.REACT_APP_FIRESTORE_PROD_AUTH_DOMAIN,
@@ -12,8 +11,9 @@ const firebaseProdConfig = {
   messagingSenderId: process.env.REACT_APP_FIRESTORE_PROD_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIRESTORE_PROD_APP_ID
 };
-*/
 
+// Development App
+/*
 const firebaseDevConfig = {
   apiKey: process.env.REACT_APP_FIRESTORE_DEV_PERSONAL_SITE_KEY,
   authDomain: process.env.REACT_APP_FIRESTORE_DEV_AUTH_DOMAIN,
@@ -23,8 +23,9 @@ const firebaseDevConfig = {
   messagingSenderId: process.env.REACT_APP_FIRESTORE_DEV_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIRESTORE_DEV_APP_ID
 };
+*/
 
-firebase.initializeApp(firebaseDevConfig);
+firebase.initializeApp(firebaseProdConfig);
 firebase.firestore();
 
 export default firebase;
