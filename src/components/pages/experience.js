cf01bd98a4ea0961796062039f879ef3f7250796
Jason Rahm <jasonrahm@gmail.com>
2022-05-20 10:23:50 -0600

    experience page styled

--- src/components/pages/experience.js
+++ src/components/pages/experience.js
@@ -1,6 +1,15 @@
 import { compose } from 'redux';
 import { connect } from 'react-redux';
 import { firestoreConnect, isLoaded } from 'react-redux-firebase'; 
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
 
 import Date from '../base/date';
 import Loading from '../base/loading';
@@ -11,8 +20,8 @@ function Experience(props) {
   const skillHeader = "Skills and Technology Used";
 
   return(
-    <main>
-      <h1>My Experience</h1>
+    <Container component="main">
+      <Typography variant="h1" component="h1">My Experience</Typography>
       { isLoaded(jobs, skills) ? (
         Object.keys(jobs).map(sectionKey => {
           let job = jobs[sectionKey];
@@ -20,44 +29,72 @@ function Experience(props) {
             return {...skills[skill.id], id: skill.id};
           });
           return (
-            <section key={sectionKey}>
-              <div>
-                <header>
-                  <h2>{job.employer}</h2>
-                  <h3>{job.title}</h3>
-                  <p>
+            <Card 
+              key={sectionKey} 
+              component="section" 
+              raised={true}
+              sx={{
+                marginBottom: '3rem'
+              }}
+            >
+              <CardContent>
+                <Box component="header">
+                  <Typography variant="h2" component="h2">{job.employer}</Typography>
+                  <Typography variant="h3" component="h3">{job.title}</Typography>
+                  <Typography paragraph={true}>
                     {job.start ? <Date date={job.start} format='MMMM YYYY' /> : ''} - {job.end ? <Date date={job.end} format='MMMM YYYY' /> : 'Present'}
-                  </p>
-                </header>
-                {job.description && job.description.map((paragraph, id) => <p key={id}>{paragraph}</p>)}
+                  </Typography>
+                </Box>
+                {job.description && job.description.map((paragraph, id) => (
+                  <Typography paragraph={true} key={id}>{paragraph}</Typography>
+                ))}
                 {job.skills && job.skills.length ? (
-                  <section>
-                    <h3>
+                  <Box component="section">
+                    <Typography variant="h6" component="h3" sx={{color: 'secondary.main'}}>
                       <span>{skillHeader}</span>
-                      <span> at {job.employer}</span>
-                    </h3>
-                    <ul aria-label={skillHeader + ' at ' + job.employer}>
+                      <span className='sr-only'> at {job.employer}</span>
+                    </Typography>
+                    <List 
+                      aria-label={skillHeader + ' at ' + job.employer}
+                      sx={{
+                        display: 'flex',
+                        justifyContent: 'flex-start',
+                        flexWrap: 'wrap'
+                      }}
+                    >
                       {jobSkills
                         .sort((a, b) => a.title.toLowerCase() > b.title.toLowerCase() ? 1 : -1)
                         .map(skill => {
                           return (
-                            <li data-job-id={sectionKey} data-skill-id={skill.id} title={skill.title} key={sectionKey + '-' + skill.id}>
-                              <span>{skill.title}</span>
-                              <img aria-hidden="true" src={skill.logo} alt=""/>
-                            </li>
+                            <ListItem 
+                              key={sectionKey + '-' + skill.id}
+                              sx={{
+                                width: 'auto'
+                              }}
+                            >
+                              <Box
+                                component="img" 
+                                src={skill.logo} 
+                                alt={skill.title}
+                                loading="lazy"
+                                sx={{
+                                  width: 35
+                                }}
+                              />
+                            </ListItem>
                           )
                       })}
-                    </ul>
-                  </section>
+                    </List>
+                  </Box>
                 ) : ''}
-              </div>
-            </section>
+              </CardContent>
+            </Card>
           )
         })
       ) : (
         <Loading />
       )}
-    </main>
+    </Container>
   )
 }
 
